aa16de794d9b250df5eb6320a68cc3376af4e866
Alif Dhimas <alifdhimasctr@gmail.com>
2024-06-19 13:36:01 +0700

    update alif 19/6

---
+++ app/src/main/java/com/example/dietnutrilyze/data/FirstDataActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dietnutrilyze.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dietnutrilyze.R
+
+class FirstDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_data)
+    }
+}
